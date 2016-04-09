f99b27fbed42d728253d324a9c2565639b6a4046
Ted Won <tkwon09@gmail.com>
2016-04-08 22:22:58 -0700

    Added rotation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,20 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
 
     public float moveSpeed;
 
     private Rigidbody2D rbody;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rbody = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
         Vector2 inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+
+        // Raycast Mouse Looking
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit = new RaycastHit();
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            Vector3 hitPoint = hit.point;
+            Vector3 targetDir = hitPoint - transform.position;
+
+            float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
         rbody.velocity = inputVector * moveSpeed * Time.fixedDeltaTime;
-	}
+    }
 }
