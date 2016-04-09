b782ff6a123018f6c527c5c8f748085227fa7876
boxofun1 <kaylab@dhcp-v016-140.mobile.uci.edu>
2016-04-08 22:58:52 -0700

    more Cthulhu updates

--- Assets/Scripts/CthulhuController.cs
+++ Assets/Scripts/CthulhuController.cs
@@ -3,23 +3,55 @@ using System.Collections;
 
 public class CthulhuController : MonoBehaviour {
 
-	public float health;
-	public BoxCollider2D theBox;
-	public SpriteRenderer sr;
+	private int hits;
+	public float speed;
 
 	void Awake ()
 	{
-		health = 100.0f;
-		theBox.enabled = true;
+		hits = 0;
+
 
 	}
 	// Use this for initialization
 	void Start () {
-	
+		InvokeRepeating ("chooseDirection", 2f, 2f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnTriggerEnter2D(Collider2D lightCollision)
+	{
+		if (lightCollision.gameObject.tag == "Light") {
+			//do stuff
+
+		}
+	}
+
+	void OnCollisionEnter2D(Collider2D anything)
+	{
+		//change direction
+	}
+
+	public void shootMonster()
+	{
+
+	}
+
+	private void getScared()
+	{
+		speed = 40;
+	}
+
+	private void chooseDirection()
+	{
+
+	}
+
+	private void move()
+	{
+
 	}
 }
