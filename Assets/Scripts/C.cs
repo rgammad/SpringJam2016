e6de4b1223d78303d802a3a2d985167177775ec6
boxofun1 <kaylab@dhcp-v016-140.mobile.uci.edu>
2016-04-08 21:18:37 -0700

    added prefab folder and cthulhu script

--- Assets/Scripts/C.cs
+++ Assets/Scripts/C.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MovementScript : MonoBehaviour {
+public class NewBehaviourScript : MonoBehaviour {
 
     public float moveSpeed;
 
---
+++ Assets/Scripts/Cthulhu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+
+	public float health;
+	public BoxCollider2D theBox;
+	public SpriteRenderer;
+
+	void Awake ()
+	{
+
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
