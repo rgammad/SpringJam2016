84a68c4370bccb513475b2cbc249743fda2583a3
boxofun1 <kaylab@dhcp-v016-140.mobile.uci.edu>
2016-04-09 12:52:36 -0700

    Added sprite and rotation for Cthulhu

--- Assets/Scripts/CthulhuController.cs
+++ Assets/Scripts/CthulhuController.cs
@@ -61,10 +61,15 @@ public class CthulhuController : MonoBehaviour {
 		
 		if (scared)
 			direction = -direction;
+
+
 	}
 
 	private void move()
 	{
 		this.transform.position += direction * speed * Time.deltaTime;
+		float angle = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+		//this.transform.rotation = Quaternion.LookRotation (direction);
 	}
 }
