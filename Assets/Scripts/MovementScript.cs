1bccb49dc1693173820b6a663d9c1fd6df6af286
Ted Won <tkwon09@gmail.com>
2016-04-08 21:09:03 -0700

    Pushing basic movement

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class MovementScript : MonoBehaviour {
 
+    public float moveSpeed;
+
+    private Rigidbody2D rbody;
+
 	// Use this for initialization
 	void Start () {
-	
+        rbody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+        Vector2 inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        rbody.velocity = inputVector * moveSpeed * Time.fixedDeltaTime;
 	}
 }
