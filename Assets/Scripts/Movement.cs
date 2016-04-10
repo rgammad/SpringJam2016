7f55bdae544cffa95c16265e1fe3f4d3da22a490
Ted Won <tkwon09@gmail.com>
2016-04-09 19:33:32 -0700

    Removed rotation from the sprite and added all the animationsoidf osijdouhwo480423urjoiwergw

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,15 +3,18 @@ using System.Collections;
 
 public class Movement : MonoBehaviour
 {
-
     public float moveSpeed;
 
     private Rigidbody2D rbody;
+    private Animator anim;
+    private float angle;
 
     // Use this for initialization
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        angle = -90;
     }
 
     // Update is called once per frame
@@ -20,10 +23,63 @@ public class Movement : MonoBehaviour
         Vector2 inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
         rbody.velocity = inputVector * moveSpeed * Time.deltaTime;
 
+        // Tell the animator which way the player is walking
+        bool walkingHoriz = true;
+        bool walkingVert = true;
+        if (inputVector.x > 0.0f)
+        {
+            anim.SetInteger("walkdir", 1);
+        }
+        else if (inputVector.x < 0.0f)
+        {
+            anim.SetInteger("walkdir", 3);
+        }
+        else
+        {
+            walkingHoriz = false;
+        }
+        if (inputVector.y > 0.0f)
+        {
+            anim.SetInteger("walkdir", 2);
+        }
+        else if (inputVector.y < 0.0f)
+        {
+            anim.SetInteger("walkdir", 0);
+        }
+        else
+        {
+            walkingVert = false;
+        }
+        anim.SetBool("walking", walkingHoriz || walkingVert);
+
         // Rotate using mouse position
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 targetDir = mousePos - transform.position;
-        float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
+    }
+
+    void Update()
+    {
+        // Tell the animator which way the torso is facing
+        if (angle <= 45 && angle > -45)
+        {
+            // RIGHT
+            anim.SetInteger("facing", 1);
+        }
+        else if (angle <= -45 && angle > -135)
+        {
+            // FORWARD
+            anim.SetInteger("facing", 0);
+        }
+        else if (angle <= 135 && angle > 45)
+        {
+            // BACK
+            anim.SetInteger("facing", 2);
+        }
+        else if (angle <= -135 || angle > 135)
+        {
+            // LEFT
+            anim.SetInteger("facing", 3);
+        }
     }
 }
