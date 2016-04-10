89496d7354084bae81d530036bcaf546db284ea6
boxofun1 <boxofun1@hotmail.com>
2016-04-10 10:41:45 -0700

    created small monster crap

---
+++ Assets/Scripts/SmallController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmallController : MonoBehaviour {
+
+	private bool activate;
+	private Vector3 playerPos;
+	public float speed;
+	private Vector3 direction;
+
+	void Awake()
+	{
+		activate = false;
+		direction = new Vector3 (0, 0, 0);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
