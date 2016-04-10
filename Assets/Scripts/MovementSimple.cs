29bcbacafbe50183a9e7275e4cb2255b0841c96d
Ted Won <tkwon09@gmail.com>
2016-04-10 10:53:43 -0700

    Added MovementSimple and a new prefab structure for player. also some dummy splashscreens

---
+++ Assets/Scripts/MovementSimple.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementSimple : MonoBehaviour {
+
+    public float moveSpeed;
+    public Transform spritetrans;
+
+    private Rigidbody2D rbody;
+
+	// Use this for initialization
+	void Start () {
+        rbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        // Get movement
+        Vector2 inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        rbody.velocity = inputVector * moveSpeed;
+
+        // Rotate using mouse position
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        spritetrans.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+	}
+}
