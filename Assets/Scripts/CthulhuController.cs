03278b9e3e11f0fb7f222bcca48c703edb587322
boxofun1 <kaylab@dhcp-v016-140.mobile.uci.edu>
2016-04-09 17:55:25 -0700

    Light penalty function

--- Assets/Scripts/CthulhuController.cs
+++ Assets/Scripts/CthulhuController.cs
@@ -10,18 +10,25 @@ public class CthulhuController : MonoBehaviour {
 	public float speed;
 	private Vector3 direction;
 	private Vector3 playerPos;
+	private bool beingStaredAt;
+	public float timeTillPenalty;
+	private float timeTillPenaltyReset;
+	private bool penalize = false;
 
 	void Awake ()
 	{
 		hits = 0;
 		direction = playerPos - this.transform.position;
 		playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+		beingStaredAt = false;
+		timeTillPenaltyReset = timeTillPenalty;
 
 
 	}
 	// Use this for initialization
 	void Start () {
 		//InvokeRepeating ("chooseDirection", 2f, 2f);
+
 	}
 	
 	// Update is called once per frame
@@ -33,19 +40,36 @@ public class CthulhuController : MonoBehaviour {
 		playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
 		chooseDirection ();
 		move ();
+
+		if (beingStaredAt)
+			timeTillPenalty -= Time.deltaTime;
+
+		if (timeTillPenalty <= 0.0)
+			penalize = true;
+
+
 	}
 
 	void OnTriggerEnter2D(Collider2D lightCollision)
 	{
 		if (lightCollision.gameObject.tag == "Light") {
-			//do stuff
+			beingStaredAt = true;
+		}
+	}
 
+	void OnTriggerExit2D(Collider2D lightCollision)
+	{
+		if (lightCollision.gameObject.tag == "Light") {
+
+			timeTillPenalty = timeTillPenaltyReset;
+			beingStaredAt = false;
+			penalize = false;
 		}
 	}
 
 	void OnCollisionEnter2D(Collision2D anything)
 	{
-		chooseDirection ();
+		//chooseDirection ();
 	}
 
 	public void shootMonster()
@@ -81,6 +105,17 @@ public class CthulhuController : MonoBehaviour {
 	private Vector3 pathAround(Vector3 target)
 	{
 		Vector3 tangent = Vector3.Cross (target, Vector3.forward);
+
+		int numTests = 5;
+		float min = Mathf.Infinity;
+
+//		for (int i = 0; i < numTests; ++i) {
+//
+//
+//			if (runGap < min)
+//				min = runGap;
+//		}
+
 		return tangent;
 	}
 
@@ -88,5 +123,10 @@ public class CthulhuController : MonoBehaviour {
 	{
 		this.transform.position += direction * speed * Time.deltaTime;
 
+	}
+
+	public bool penalty()
+	{
+		return penalize;
 	}
 }
